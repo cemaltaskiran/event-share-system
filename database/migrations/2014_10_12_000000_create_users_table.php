1f5365b191063e1957140a7adf99b7d353211d25
cemaltaskiran <cemaltaskiran@gmail.com>
2017-04-08 18:05:36 +0300

    Initialization

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateOrganizersTable extends Migration
+class CreateUsersTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,11 @@ class CreateOrganizersTable extends Migration
      */
     public function up()
     {
-        Schema::create('organizers', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');                        
+            $table->string('name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->string('address');
-            $table->boolean('is_active');
-            $table->boolean('is_in_penalty');
             $table->rememberToken();
             $table->timestamps();
         });
@@ -33,6 +30,6 @@ class CreateOrganizersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('organizers');
+        Schema::dropIfExists('users');
     }
 }
